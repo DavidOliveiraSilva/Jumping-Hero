af84f7ac2d88aa9aca996e3e8e4f170772535bb7
David Oliveira <clear844@gmail.com>
2019-05-26 02:49:15 -0300

    third commit

--- Jumping Hero/Assets/Plataforma.cs
+++ Jumping Hero/Assets/Plataforma.cs
@@ -13,6 +13,10 @@ public class Plataforma : MonoBehaviour
     private GameObject player;
     //[/gambis]
 
+    public GameObject[] coisas;
+    [Range(0.0f, 1.0f)]
+    public float probabilidade;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +26,22 @@ public class Plataforma : MonoBehaviour
         //[gambis]
         player = GameObject.Find("Player");
         //[/gambis]
+        if (coisas.Length > 0) {
+            int posicaoLivre = Random.Range(0, 6);
+            for (int i = 0; i < 12; i++) {
+                if (i != posicaoLivre && i != posicaoLivre + 6) {
+                    if (Random.Range(0.0f, 1.0f) < probabilidade) {
+                        int j = Random.Range(0, coisas.Length);
+                        GameObject coisa = Instantiate(coisas[j]);
+                        coisa.transform.position = transform.GetChild(i).position;
+                        if(i >= 6) {
+                            coisa.transform.eulerAngles = new Vector3(0, 0, 180);
+                        }
+                        coisa.transform.SetParent(transform);
+                    }
+                }
+            }
+        }
     }
 
     // Update is called once per frame
@@ -29,7 +49,7 @@ public class Plataforma : MonoBehaviour
     {
         //[gambis]
         if(player != null) {
-            if(player.transform.position.y > transform.position.y + 1) {
+            if(player.transform.position.y > transform.position.y + 0.55f) {
                 bc.enabled = true;
             } else {
                 bc.enabled = false;
